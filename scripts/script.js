b4869d6a99a7925c626c8075d9afb18ef0dbf372
Esther Ndosi <estherndosi24@gmail.com>
2025-10-15 14:43:06 +0300

    Adding updating, deleting functionalities

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,51 @@
 import { Project, Todo } from "../modal/modals.js"
-import { retrieve, save } from "../service/local_storage.js"
-
-let projects = []
-let todos = []
+import { deleteItem, update, retrieve, save } from "../service/local_storage.js"
 
 const proj1 = new Project('Todo Application', 'An application which saves todos')
-projects.push(proj1)
-save('projects', JSON.stringify(projects))
+save(proj1, 'projects')
 const proj2 = new Project('Mic Courier Website', 'A static website which display information')
-projects.push(proj2)
-save('projects', JSON.stringify(projects))
+save(proj2, 'projects')
+
+let projects = retrieve('projects')
 
 const todo1 = new Todo(projects[0].id, 'Prepare pseudocode', '11/10/2025', 'High')
-todos.push(todo1)
-save('todos', JSON.stringify(todos))
+save(todo1)
 const todo2 = new Todo(projects[1].id, 'Set a project scheleton', '11/10/2025', 'Medium')
-todos.push(todo2)
-save('todos', JSON.stringify(todos))
+save(todo2)
+const todo3 = new Todo(projects[0].id, 'Prepare Designs', '16/10/2025', 'Medium')
+save(todo3)
+let todos = retrieve('todos')
 
-projects = JSON.parse(retrieve('projects'))
-todos = JSON.parse(retrieve('todos'))
+function printReport() {
+    projects = retrieve('projects')
+    todos = retrieve('todos')
+    projects.forEach(project => {
+        console.log(`Project Title: ${project.title}`)
+        console.log('Todos')
+        const projectTodos = todos.filter(todo => todo.projectId === project.id)
+        projectTodos.forEach(todo => {
+            console.log(`Title: ${todo.title} due on ${todo.dueDate}`)
+        })
+    });
+}
+console.log('Start')
+printReport()
 
-projects.forEach(project => {
-    console.log(`Project Title: ${project.title}`)
-    console.log('Todos')
-    const projectTodos = todos.filter(todo => todo.projectId === project.id)
-    projectTodos.forEach(todo => {
-        console.log(`Title: ${todo.title} due on ${todo.dueDate}`)
-    })
-});
+update('projects', projects[0].id, {title:'I have changed a project', description: 'do you see me?'})
+
+console.log('After updating a project')
+printReport()
+
+update('todos', todos[1].id, {title:'I have changed a todo', description: 'do you see me as a todo?'})
+console.log('After updating a todo')
+printReport()
+
+deleteItem('projects', projects[1].id)
+
+console.log('After deleting a project')
+printReport()
+
+deleteItem('todos', todos[0].id)
+
+console.log('After deleting a todo')
+printReport()
--- service/local_storage.js
+++ service/local_storage.js
@@ -1,7 +1,45 @@
-export const save = (key, value) => {
-    localStorage.setItem(key, value)
+let projects = []
+let todos = []
+
+
+export const save = (data, type='todos') => {
+    if (type === 'todos') {
+        todos.push(data)
+        localStorage.setItem(type, JSON.stringify(todos))
+    } else {
+        projects.push(data)
+        localStorage.setItem(type, JSON.stringify(projects))
+    }
+    
 }
 
 export const retrieve = (key) => {
-    return localStorage.getItem(key)
+    return JSON.parse(localStorage.getItem(key))
+}
+
+export const update = (key, id, data) => {
+    if (key === 'todos') {
+        const index = todos.findIndex(todo => todo.id === id)
+        todos[index].title = data.title
+        todos[index].dueDate = data.dueDate
+        todos[index].priority = data.priority
+        localStorage.setItem(key, JSON.stringify(todos))
+    } else {
+        const index = projects.findIndex(project => project.id === id)
+        projects[index].title = data.title
+        projects[index].description = data.description
+        localStorage.setItem(key, JSON.stringify(projects))
+    }
+}
+
+export const deleteItem = (key, id) => {
+    if (key === 'todos') {
+        todos = todos.filter(todo => todo.id !== id)
+        localStorage.setItem(key, JSON.stringify(todos))
+    } else {
+        todos = todos.filter(todo => todo.projectId !== id)
+        projects = projects.filter(project => project.id !== id)
+        localStorage.setItem('todos', JSON.stringify(todos))
+        localStorage.setItem(key, JSON.stringify(projects))
+    }
 }
